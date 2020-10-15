50d8b40a1899af5ce12d236ed8fb524b35b14766
Julia <juliyanevar@gmail.com>
2020-10-15 20:28:43 +0300

    Initial commit

---
+++ src/main/java/by/nevar/theatre/ServletInitializer.java
@@ -0,0 +1,13 @@
+package by.nevar.theatre;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TheatreApplication.class);
+	}
+
+}
---
+++ src/test/java/by/nevar/theatre/TheatreApplicationTests.java
@@ -0,0 +1,13 @@
+package by.nevar.theatre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheatreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
