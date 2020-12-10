0001d5575584c9bca5c10386871bda3d0d46fa3c
Julia <juliyanevar@gmail.com>
2020-12-11 01:18:43 +0300

    add Security

---
+++ src/main/java/by/nevar/theatre/security/JwtAuthenticationException.java
@@ -0,0 +1,14 @@
+package by.nevar.theatre.security;
+
+import org.springframework.security.core.AuthenticationException;
+
+
+public class JwtAuthenticationException extends AuthenticationException {
+    public JwtAuthenticationException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+    public JwtAuthenticationException(String msg) {
+        super(msg);
+    }
+}
