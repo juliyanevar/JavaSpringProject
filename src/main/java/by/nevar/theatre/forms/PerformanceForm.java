99b589e7108ba3a53abe2b3430e07800b676f9e6
Julia <juliyanevar@gmail.com>
2020-12-10 20:37:21 +0300

    add Forms

--- src/main/java/by/nevar/theatre/forms/PerformanceForm.java
+++ src/main/java/by/nevar/theatre/forms/PerformanceForm.java
@@ -1,10 +1,12 @@
 package by.nevar.theatre.forms;
 
 import by.nevar.theatre.models.Actor;
+import by.nevar.theatre.models.Theater;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.sql.Time;
 import java.util.Date;
 import java.util.List;
@@ -14,8 +16,11 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PerformanceForm {
-    private String name;
+    private Integer id;
+    private String Title;
+    private String Genre;
+    private Theater theater;
     private Date date;
     private Time time;
-    private List<Actor> listActors;
+    List<Actor> actors;
 }
--- src/main/java/by/nevar/theatre/forms/TheaterForm.java
+++ src/main/java/by/nevar/theatre/forms/TheaterForm.java
@@ -1,14 +1,16 @@
 package by.nevar.theatre.forms;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserForm {
+public class TheaterForm {
     private Integer id;
-    private String username;
-    private String password;
+    private String name;
 }
